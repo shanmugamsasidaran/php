682f8f91ba6a42dd51be5a374f6f28c74005e5d6
Sasidaran <101876060+shanmugamsasidaran@users.noreply.github.com>
2022-04-26 12:12:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
 <?php
   include "test.php";
   echo "index page";
-  echo "1";
-  echo "2";
-  echo "3";
-  echo "4";
-  echo "5";
-  echo "6";
-  echo "7";
-  echo "8";
-  echo "9";
-  echo "10";
-  echo "11";
-  echo "12";
-  echo "13";
-  echo "14";
-  echo "15";
-  echo "16";
-  echo "17";
-  echo "18";
-  echo "19";
-  echo "20";
+  for (i=0; $i<20; $i++)
+    echo $i."<br>";
