b4767ac55a613eca2daa562daa5c9c95e68943d5
Sasidaran <101876060+shanmugamsasidaran@users.noreply.github.com>
2022-04-26 13:10:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,19 +3,3 @@
   echo "index page";
   for (i=0; $i<20; $i++)
     echo $i."<br>";
-  for (i=0; $i<20; $i++)
-    echo $i."<br>";
-  for (i=0; $i<20; $i++)
-    echo $i."<br>";
-  for (i=0; $i<20; $i++)
-    echo $i."<br>";
-  for (i=0; $i<20; $i++)
-    echo $i."<br>";
-  for (i=0; $i<20; $i++)
-    echo $i."<br>";
-  for (i=0; $i<20; $i++)
-    echo $i."<br>";
-  for (i=0; $i<20; $i++)
-    echo $i."<br>";
-  for (i=0; $i<20; $i++)
-    echo $i."<br>";
