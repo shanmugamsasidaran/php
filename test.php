5d1a0afb965cb5b56d5d902e6f103298c455b152
Sasidaran <101876060+shanmugamsasidaran@users.noreply.github.com>
2022-04-26 15:59:14 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,5 @@
 <?php
-  $cars = array("Volvo", "BMW", "Toyota");
-  echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+  function print_array($cars) {
+    return "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+  }
 ?>
